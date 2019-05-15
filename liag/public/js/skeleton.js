1f4e52b66753062143098b42350200d1ee894a39
李欣 <JustinDellAdam@live.com>
2019-05-15 23:26:45 +0800

    models and json lib import and framework init

---
+++ liag/public/js/skeleton.js
@@ -0,0 +1,7 @@
+/**
+ * LiXin Avatar Generator
+ *
+ * Based on https://github.com/mrdoob/three.js/
+ * Tested on r94
+ * @author lixin / https://github.com/Great-Li-Xin
+ */
--- liag/src/App.js
+++ liag/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserView, MobileView} from "react-device-detect";
+import Typed from "react-typed";
+
+import {library} from "@fortawesome/fontawesome-svg-core";
+
+// fontawesome imports
+import {faTimesCircle} from "@fortawesome/free-solid-svg-icons";
+import {faDollarSign} from "@fortawesome/free-solid-svg-icons";
+import {faLink} from "@fortawesome/free-solid-svg-icons";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+
+// Loading assets
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+
+// Loading the data this way for now
+import data from "./library/category.json";
+
+// Loading the different components
+import Name from "./components/Name";
+import Footer from "./components/Footer";
+import Category from "./components/Category";
+import Buttons from "./components/Buttons";
+import Popup from "./components/Popup";
+import PageLoader from "./components/PageLoader";
+import PartLoader from "./components/PartLoader";
+import MyGithub from "./components/MyGithub";
+
+library.add(faTimesCircle);
+library.add(faDollarSign);
+library.add(faLink);
+library.add(faSearch);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
