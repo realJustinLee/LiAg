8cf16fd3c5e76eda06cd28272552e2e7ad5df0cb
李欣 <JustinDellAdam@live.com>
2019-12-05 15:42:37 +0800

    minor fix

--- liag/public/js/vendor/three/exporters/STLExporter.js
+++ liag/public/js/vendor/three/exporters/STLExporter.js
@@ -1,7 +1,7 @@
 /**
  * Based on https://github.com/mrdoob/three.js/examples/js/exporters/STLExporter.js
  *
- * Tested on r104
+ * Tested on r110
  *
  * @author kjlubick / https://github.com/kjlubick
  * @author kovacsv / http://kovacsv.hu/
